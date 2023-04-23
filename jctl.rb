785fbdc92dbe97724cc8fc3ccc9919a57addcf09
Jonas Vadebo <jonas.vadebo@king.com>
2023-04-24 00:02:33 +0200

    Initial commit

---
+++ jctl.rb
@@ -0,0 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Jctl < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/jonasvadebo/jclt/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "a2d74c6664bb0418066600de8746fdce1d2f00742c9088dc8ae0ddc7e3fa3e3b"
+  license ""
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test jctl`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
